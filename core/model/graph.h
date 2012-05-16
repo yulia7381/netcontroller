a05221ef748c5e4688867df21c3ca7835a2b9d61
Vladislav Dolbilov <darl@yandex-team.ru>
2012-05-16 14:49:14 +0400

    includes

--- core/model/graph.h
+++ core/model/graph.h
@@ -1,6 +1,8 @@
 #ifndef GRAPH_H
 #define GRAPH_H
 
+#include <list>
+
 #include "link.h"
 
 class Graph {
--- core/model/node.h
+++ core/model/node.h
@@ -1,6 +1,8 @@
 #ifndef NODE_H
 #define NODE_H
 
+#include <string>
+
 #include "node_type.h"
 
 class Node {

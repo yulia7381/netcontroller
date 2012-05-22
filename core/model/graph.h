e94df5ddf92e5f42024036b550a3c57286fb7689
Vladislav Dolbilov <darl@yandex-team.ru>
2012-05-22 04:04:34 +0400

    new graph model with templates

--- core/model/graph.h
+++ core/model/graph.h
@@ -3,21 +3,50 @@
 
 #include <list>
 
-#include "link.h"
+namespace core {
+    template<class Node, class Link>
+    class NodeLink {
+    public:
+        NodeLink() {};
+        NodeLink(const NodeLink& lnk) : node1(lnk.node1), node2(lnk.node2), link(lnk.link) {};
+        NodeLink(Node n1, Node n2, Link l) : node1(n1), node2(n2), link(l) {};
+        NodeLink& operator = (const NodeLink& lnk) {
+            node1 = lnk.node1;
+            node2 = lnk.node2;
+            link = lnk.link;
+            return this;
+        };
 
-class Graph {
-public:
-    Graph(const std::list<Link*>& links) : links(links) {};
-    Graph(const Graph& gr) : links(gr.links) {};
+        Node getNode1();
+        Node getNode2();
 
-    const std::list<Link*>& getLinks() const {return links;};
+        Link getLink();
+    private:
+        Node node1;
+        Node node2;
+        Link link;
+    };
 
-    bool containsNode(unsigned long id) const;
-    bool containsLink(unsigned long id) const;
+    template<class Node, class Link>
+    class Graph {
+    public:
+        Graph() {};
+        Graph(const Graph& gr);
 
-    Graph& operator= (const Graph& gr);
-private:
-    std::list<Link*> links;
-};
+        Graph& operator= (const Graph& gr);
+        
+        std::list<Node> getNodes() const {return nodes;};
+        std::list<NodeLink<Node, Link> > getLinks() const {return links;};
+
+        std::list<NodeLink<Node, Link> > getLinksFromNode(Node node);
+        Link getLink(Node node1, Node node2);
+
+        void addNode(Node node);
+        void addLink(Node node1, Node node2, Link link);
+    private:
+        std::list<Node> nodes;
+        std::list<NodeLink<Node, Link> > links;
+    };
+}
 
 #endif
